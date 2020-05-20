66be604a728341a088f8ec3744eeee7d276df3d5
Leon Marbukh <leonmarbukh@gmail.com>
2020-05-19 18:50:12 -0700

    migration error

---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,16 @@
+INSERT INTO noteful_notes (name, modified, folder_id, content)
+VALUES
+('Dogs', '2019-01-03T00:00:00.000Z', 1, 'Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. Velit ex animi reiciendis quasi.'),
+('Cats', '2018-08-15T23:00:00.000Z', 2, 'Eos laudantium quia ab blanditiis temporibus necessitatibus. Culpa et voluptas ut sed commodi. Est qui ducimus id.'),
+('Pigs', '2018-03-01T00:00:00.000Z', 3, 'Occaecati dignissimos quam qui facere deserunt quia. Quaerat aut eos laudantium dolor odio officiis illum. Velit vel qui dolorem et.\n \rQui ut vel excepturi in at. Ut accusamus cumque quia sapiente ut ipsa nesciunt. Dolorum quod eligendi qui aliquid sint.\n \rAt id deserunt voluptatem et rerum. Voluptatem fuga tempora aut dignissimos est odio maiores illo. Fugiat in ad expedita voluptas voluptatum nihil.'),
+('Birds','2019-01-04T00:00:00.000Z', 1, 'Eum culpa odit. Veniam porro molestiae dolores sunt reiciendis culpa. Atque accusamus dolore eos odio facilis.'),
+('Bears', '2018-07-12T23:00:00.000Z', 2, 'Distinctio dolor nihil ad iure quo tempore id ipsum. Doloremque sunt dicta odit. Id veritatis aut et doloremque natus.'),
+('Horses', '2018-08-20T23:00:00.000Z', 3, 'Aliquid magnam ut quis quas impedit molestiae laudantium adipisci et. Officiis ut dolor rerum molestiae.'),
+('Tigers', '2018-03-03T00:00:00.000Z', 1, 'Eaque aliquid sit. Ducimus consequatur nam. Corporis ut dolorum amet molestiae minima ut quasi nesciunt.'),
+('Wolves', '2018-05-16T23:00:00.000Z', 2, 'Expedita mollitia et. Voluptates optio expedita. Esse ullam numquam quae rem. Cum esse itaque et.'),
+('Elephants', '2018-04-11T23:00:00.000Z', 3, 'Rem enim voluptatem autem fuga possimus. Perferendis voluptatem labore vero veritatis laborum aspernatur molestiae.'),
+('Lions', '2018-04-26T23:00:00.000Z', 1, 'Occaecati qui magni blanditiis. Et animi quas. Harum quo dolore quia nam amet numquam. Omnis et ex numquam et nostrum dolores voluptatibus.'),
+('Monkeys', '2018-02-05T00:00:00.000Z', 2, 'Tenetur deleniti vero. Qui et sit voluptatem et dolor voluptas. Aut adipisci autem dolorem ad excepturi ut facere.'),
+('Bats', '2018-12-01T00:00:00.000Z', 3, 'Tempore aliquam nobis amet dolor laborum aspernatur aspernatur. Non porro est mollitia nobis. Eveniet possimus non et mollitia non.'),
+('Turtles', '2018-09-11T23:00:00.000Z', 1,'Fugiat dolores et nostrum laborum id delectus sint reiciendis. Recusandae nulla repellendus. Labore eum hic nesciunt enim corporis necessitatibus.'),
+('Zebras', '2018-08-13T23:00:00.000Z', 2, 'Veritatis porro minima perspiciatis. Repellat veniam quo iste ut. Iusto voluptas quae quibusdam. Odit neque iusto cupiditate iste quam.');
--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgresql://postgres:tapwater1@localhost/noteful",
+    API_KEY: process.env.REACT_APP_API_KEY,
 };
